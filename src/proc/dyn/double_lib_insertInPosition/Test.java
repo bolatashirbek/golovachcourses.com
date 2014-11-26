e3a4d167b6345d9178733795be019cc2edd30084
Pavel <gothica@inbox.ru>
2014-11-26 12:56:52 +0300

    added DoubleLLUtils

---
+++ src/proc/dyn/double_lib_insertInPosition/Test.java
@@ -0,0 +1,7 @@
+package proc.dyn.double_lib_insertInPosition;
+
+/**
+ * Created by p.lepeev on 26.11.14.
+ */
+public class Test {
+}
---
+++ src/proc/dyn/double_lib_removeLast/DoubleLLUtils.java
@@ -0,0 +1,7 @@
+package proc.dyn.double_lib_removeLast;
+
+/**
+ * Created by p.lepeev on 26.11.14.
+ */
+public class DoubleLLUtils {
+}
